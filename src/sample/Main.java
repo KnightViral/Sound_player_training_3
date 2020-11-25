0a4a8fe4b725cdfc1b87041b521fa0b96a6da341
KnightViral <wow2005@yandex.ru>
2020-11-25 21:04:22 +0400

    123

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Воспроизведение 2.0");
+        primaryStage.setTitle("Воспроизведение 2.1");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
